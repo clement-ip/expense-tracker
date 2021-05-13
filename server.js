a06f8bd028d707237eeeceba8cdebbae97662037
Clement Ip <clement.ip96@gmail.com>
2021-05-13 14:55:24 -0700

    cleaned up some files for production

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
@@ -18,13 +19,14 @@ if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
+app.use('/api/v1/transactions', transactions);
+
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
   
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
   }
 
-app.use('/api/v1/transactions', transactions);
 
 const PORT = process.env.PORT || 5000;
 
